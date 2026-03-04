09f5d8793500be70d5c0ddfe7643e8619e415766
Arthur Furtado Machado <arthur_machado150@estudante.sesisenai.org.br>
2026-03-04 16:45:20 -0300

    Segunda Versão

--- index.js
+++ index.js
@@ -8,4 +8,6 @@ do{
     console.log('|--- 1 = Prisma Triangular -------------------------------------|')
     console.log('|--- 2 = Pirâmide Triangular -----------------------------------|')
     console.log('|---------------------------------------------------------------|')
-}
+}while(opcao !== 0)
+
+console.log('fim do programa')
